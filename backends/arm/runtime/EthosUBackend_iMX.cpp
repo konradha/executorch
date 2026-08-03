a6bba33db35c1ddddc9c42f8ba79d0522ec62fae
Konrad <konradha@yahoo.com>
2026-08-03 14:14:54 +0200

    rm unnecessary comments

--- backends/arm/runtime/EthosUBackend_iMX.cpp
+++ backends/arm/runtime/EthosUBackend_iMX.cpp
@@ -1,14 +1,3 @@
-/*
- * Copyright 2026 Arm Limited and/or its affiliates.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
-/*
- * NXP i.MX uses a Vela model and one I/O/scratch arena through /dev/ethosu0.
- */
-
 #include <algorithm>
 #include <array>
 #include <cstdint>
--- examples/arm/ethos-u-setup/aarch64-linux-conda-toolchain.cmake
+++ examples/arm/ethos-u-setup/aarch64-linux-conda-toolchain.cmake
@@ -1,8 +1,3 @@
-# Copyright 2026 Arm Limited and/or its affiliates.
-#
-# This source code is licensed under the BSD-style license found in the
-# LICENSE file in the root directory of this source tree.
-
 if(CMAKE_VERSION VERSION_LESS 3.20)
   message(FATAL_ERROR "This toolchain file requires at least CMake 3.20")
 endif()
